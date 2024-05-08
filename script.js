d9511a4feccc6a27b45d650c7141f52eaa89fdb6
SonuuChowdhury <choudhurysonu2004@gmail.com>
2024-05-08 22:52:00 +0530

    Some more functions are added , display is optimized

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let multiplyKey=document.querySelector(".multiplyKey");
 let addKey=document.querySelector(".addKey");
 let minusKey=document.querySelector(".minusKey");
 let divideKey=document.querySelector(".divideKey");
+let KN0=document.querySelector(".KN0");
 let KN1=document.querySelector(".KN1");
 let KN2=document.querySelector(".KN2");
 let KN3=document.querySelector(".KN3");
@@ -14,23 +15,86 @@ let KN6=document.querySelector(".KN6");
 let KN7=document.querySelector(".KN7");
 let KN8=document.querySelector(".KN8");
 let KN9=document.querySelector(".KN9");
+let KNdecimalKey=document.querySelector(".decimalKey");
 let display=document.querySelector(".display");
 let delKey=document.querySelector(".delKey");
+let equalKey=document.querySelector(".equalKey");
+let resetKey=document.querySelector(".resetKey");
 
 display.value="0";
 // Variables
 let firstNumber="";
 let secoundNumber="";
 let flagIndicator=0;
+let val1=0;
+let val2=0;
+
+let operatorIndicator=0;
+// operatorIndicator values 
+// 1--> +
+// 2--> -
+// 3--> *
+// 4--> /
 
 
 // functions
 function assignValue(n){
-    firstNumber=firstNumber+n;
-    display.value=firstNumber;
+    
+    if (flagIndicator==0) {
+        firstNumber=firstNumber+n;
+        display.value=firstNumber;  
+    }
+    
+    else{
+        if (operatorIndicator==1) {
+            secoundNumber=secoundNumber+n;
+            display.value="Ans + "+secoundNumber; 
+            
+        }
+        else if(operatorIndicator==2){
+            secoundNumber=secoundNumber+n;
+            display.value="Ans - "+secoundNumber; 
+        }
+        else if(operatorIndicator==3){
+            secoundNumber=secoundNumber+n;
+            display.value="Ans x "+secoundNumber; 
+        }
+        else if(operatorIndicator==4){
+            secoundNumber=secoundNumber+n;
+            display.value="Ans / "+secoundNumber; 
+        }
 
-
+    }
 }
+
+function operatorControlInit(opr){
+    if (opr=="+") {
+        val1=display.value;
+        flagIndicator=1;  
+        operatorIndicator=1;
+        display.value="Ans +";
+        
+    }
+    else if(opr=="-"){
+        val1=display.value;
+        flagIndicator=1;  
+        operatorIndicator=2;
+        display.value="Ans -";
+    }
+    else if(opr=="*"){
+        val1=display.value;
+        flagIndicator=1;  
+        operatorIndicator=3;
+        display.value="Ans x";
+    }
+    else{
+        val1=display.value;
+        flagIndicator=1;  
+        operatorIndicator=4;
+        display.value="Ans /";
+    }
+}
+
 
 function delValue(n){
     if (n==0) {
@@ -38,19 +102,35 @@ function delValue(n){
         display.value=firstNumber;   
     }
     else{
-        secoundNumber="";
-        display.value=secoundNumber;
+        if (operatorIndicator==1) {
+            secoundNumber="";
+            display.value="Ans + "+secoundNumber;   
+        }
+        else if (operatorIndicator==2){
+            secoundNumber="";
+            display.value="Ans - "+secoundNumber;
+
+        }
+        else if (operatorIndicator==3){
+            secoundNumber="";
+            display.value="Ans x "+secoundNumber;
+        }
+
+        else if (operatorIndicator==4){
+            secoundNumber="";
+            display.value="Ans / "+secoundNumber;
+        }
     }
+}
+
+function answerControl(){
+    
 }
 
 
 
-
-
-
-
 KN1.addEventListener('click', function(){
-    assignValue("1");
+    assignValue(1);
 });
 KN2.addEventListener('click', function(){
     assignValue(2);
@@ -76,24 +156,33 @@ KN8.addEventListener('click', function(){
 KN9.addEventListener('click', function(){
     assignValue(9);
 });
+KN0.addEventListener('click', function(){
+    assignValue(0);
+});
+KNdecimalKey.addEventListener('click',function(){
+    assignValue(".");
 
+});
 delKey.addEventListener('click',function(){
     delValue(flagIndicator);
 });
 
 minusKey.addEventListener('click', function(){
-    operatorControl("-");
+    operatorControlInit("-");
 });
 addKey.addEventListener('click', function(){
-    operatorControl("+");
+    operatorControlInit("+");
 });
 multiplyKey.addEventListener('click', function(){
-    operatorControl("*");
+    operatorControlInit("*");
 });
 divideKey.addEventListener('click', function(){
-    operatorControl("/");
+    operatorControlInit("/");
 });
 
+equalKey.addEventListener('click',function(){
+    answerControl();
+});
 
 
 
